bbb51ccfde5267e4290bc44cec4a16ce477ea2d2
prashantkumar3639 <83387494+prashantkumar3639@users.noreply.github.com>
2021-05-06 10:55:42 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class PrimeNumber {
+    static boolean checkPrime(int n) {
+        if (n == 1 || n == 2)
+            return true;
+        else {
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int num = sc.nextInt();
+        System.out.println("Prime Numbers up to " +num);
+
+        for (int i = 1; i <= num; i++) {
+            
+            if (checkPrime(i) == true) {
+                System.out.print(i +" ");
+            }
+        }
+
+    }    
+}
